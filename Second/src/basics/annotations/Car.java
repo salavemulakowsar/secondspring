e7c4d171a28c635d070faf564ce120a210535c43
kowsar <salavemulak@gmail.com>
2021-04-20 11:15:49 +0530

    @component

--- Second/src/basics/annotations/Car.java
+++ Second/src/basics/annotations/Car.java
@@ -1,6 +1,11 @@
 package basics.annotations;
 
+import org.springframework.stereotype.Component;
+
+//import org.springframework.stereotype.Component;
+@Component
 public class Car implements Vehicle {
+	
 	public void drive()
 	{
 		System.out.println("Car drive");
